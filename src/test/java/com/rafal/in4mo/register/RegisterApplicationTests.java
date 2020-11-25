63cd4965a321c74a0494f5fad5afbb215d1d3ab7
Rafal ROjek <rafalrojek90@gmail.com>
2020-11-25 17:59:42 +0100

    Initial commit

---
+++ src/test/java/com/rafal/in4mo/register/RegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafal.in4mo.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
